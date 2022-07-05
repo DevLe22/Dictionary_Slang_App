e6611548f8d67c29f88f5a763829f48a8b9a9a4c
huygiale <huygiale2202@gmail.com>
2022-07-05 23:30:50 +0700

    update save history reset dictionary

--- src/Dictionary_21424025.java
+++ src/Dictionary_21424025.java
@@ -157,6 +157,273 @@ public class Dictionary_21424025 {
         Object[] convertedToArray = data_word.keySet().toArray();
         return (String)convertedToArray[new Random().nextInt(convertedToArray.length)];
     }
+    public void saveHistory(String path){
+        File file = new File(path);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            for(String i:history){
+                fileWriter.write(i+"\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveAddHistory(String path){
+        File file = new File(path);
+
+        try {
+            FileWriter fw = new FileWriter(file);
+            Set<Map.Entry<String,Set<String>>> entrySet = add_history.entrySet();
+            for(Map.Entry<String,Set<String>> indexSet:entrySet){
+                fw.write(indexSet.getKey()+"`");
+                Set <String> saved;
+                saved = indexSet.getValue();
+                int size = saved.size();
+                for(String indexString : saved){
+                    size--;
+                    if(size!=0){
+                        fw.write(indexString+"|");
+
+                    }
+                    else {
+                        fw.write(indexString+"\n");
+                    }
+                }
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void saveEditHistory(String path){
+        File file = new File(path);
+
+        try {
+            FileWriter fw = new FileWriter(file);
+            Set<Map.Entry<String,Set<String>>> entrySet = edit_history.entrySet();
+            for(Map.Entry<String,Set<String>> indexSet:entrySet){
+                fw.write(indexSet.getKey()+"`");
+                Set <String> saved;
+                saved = indexSet.getValue();
+                int size = saved.size();
+                for(String indexString : saved){
+                    size--;
+                    if(size!=0){
+                        fw.write(indexString+"|");
+
+                    }
+                    else {
+                        fw.write(indexString+"\n");
+                    }
+                }
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void saveDeleteHistory(String path){
+        File file = new File(path);
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            for(String i:delete_history){
+                fileWriter.write(i+"\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void addSlangWord(){
+        System.out.print("Enter slang word: ");
+        String slangWord = scanner.nextLine();
+        slangWord = slangWord.toUpperCase();
+        Set <String>def = new HashSet<>();
+        boolean isContain = data_word.containsKey(slangWord);
+
+        if(!isContain){
+
+            int numberOfDef = 0;
+            while(true){
+                try{
+                    System.out.print("Enter the number of mean (0 < number of mean < 6): ");
+                    String str = scanner.nextLine();
+                    numberOfDef = Integer.parseInt(str);
+                    if(numberOfDef<=0||numberOfDef>5){
+                        System.out.println("The value must be less than 6 and more than 0!!");
+                        continue;
+                    }
+                    break;
+                }
+                catch (NumberFormatException ex){
+                    System.out.println("The input must be integer, please enter again!!!");
+                }
+            }
+
+            for(int i = 0;i<numberOfDef;i++){
+                System.out.print("Enter definition "+ (i+1)+": ");
+                String tempInput = scanner.nextLine();
+                def.add(tempInput);
+
+            }
+
+            data_word.put(slangWord,def);
+            add_history.put(slangWord,def);
+        }
+        else {
+
+            int check = 0;
+            while (true){
+                System.out.println("This slang word already exist in this dictionary, please enter the next action");
+                System.out.println("Overwrite it, Enter 1");
+                System.out.println("Duplicate it, Enter 2");
+                System.out.print("Enter your choice: ");
+                try{
+                    check = Integer.parseInt(scanner.nextLine());
+                }
+                catch(NumberFormatException ex){
+                    System.out.println("You must enter an integer!!, please enter again");
+                }
+                if(check<1||check>2){
+                    continue;
+                }
+                break;
+
+            }
+            int numberOfDef = 0;
+            while(true){
+                try{
+                    System.out.print("Enter the number of mean (0 < number of mean < 6): ");
+                    String str = scanner.nextLine();
+                    numberOfDef = Integer.parseInt(str);
+                    if(numberOfDef<=0||numberOfDef>5){
+                        System.out.println("The value must be less than 6 and more than 0!!");
+                        continue;
+                    }
+                    break;
+                }
+                catch (NumberFormatException ex){
+                    System.out.println("The input must be integer, please enter again!!!");
+                }
+            }
+
+            for(int i = 0;i<numberOfDef;i++){
+                System.out.print("Enter definition "+ (i+1)+": ");
+                String tempInput = scanner.nextLine();
+                def.add(tempInput);
+
+            }
+            if(check==1){
+                data_word.put(slangWord,def);
+            }
+            else {
+                Set<String> oldValues = data_word.get(slangWord);
+                def.addAll(oldValues);
+                data_word.put(slangWord,def);
+            }
+
+        }
+    }
+    public void updateSlangWord(){
+        System.out.print("Enter the slang word you want to edit: ");
+        String word = scanner.nextLine();
+        if(data_word.containsKey(word)){
+            //
+            //
+            Set <String>def = new HashSet<>();
+            int numberOfDef = 0;
+            while(true){
+                try{
+                    System.out.print("Enter the number of mean (0 < number of mean < 6): ");
+                    String str = scanner.nextLine();
+                    numberOfDef = Integer.parseInt(str);
+                    if(numberOfDef<=0||numberOfDef>5){
+                        System.out.println("The value must be less than 6 and more than 0!!");
+                        continue;
+                    }
+                    break;
+                }
+                catch (NumberFormatException ex){
+                    System.out.println("The input must be integer, please enter again!!!");
+                }
+            }
+
+            for(int i = 0;i<numberOfDef;i++){
+                System.out.print("Enter definition "+ (i+1)+": ");
+                String tempInput = scanner.nextLine();
+                def.add(tempInput);
+
+            }
+            data_word.put(word,def);
+            edit_history.put(word,def);
+        }
+        else {
+            System.out.println("word not exist in this dictionary !!!");
+        }
+
+
+    }
+    public boolean deleteSlangWord(){
+        System.out.println("Enter slang word you want to delete");
+        String word = scanner.nextLine();
+
+        if(data_word.containsKey(word)){
+            int num=0;
+            while(true){
+                try{
+                    System.out.print("Do you want to delete this slang word (1)YES, (2)NO: ");
+                    String str = scanner.nextLine();
+                    num = Integer.parseInt(str);
+                }
+                catch (NumberFormatException ex){
+                    System.out.println("You must enter a integer, please enter again!!!");
+                }
+                if(num<1||num>2){
+                    continue;
+                }
+                break;
+            }
+            if(num==1){
+                word = word.toUpperCase();
+                if(data_word.remove(word)!=null){
+                    delete_history.add(word);
+                    return true;
+                }
+
+            }
+        }
+        System.out.println("Slang word does not exist in this dictionary");
+
+        return false;
+    }
+    public void resetDictionary(){
+        int num=0;
+        while(true){
+            try{
+                System.out.print("Do you want to reset this dictionary (1)YES, (2)NO: ");
+                String str = scanner.nextLine();
+                num = Integer.parseInt(str);
+            }
+            catch (NumberFormatException ex){
+                System.out.println("You must enter a integer, please enter again!!!");
+            }
+            if(num<1||num>2){
+                System.out.println("The input must be 1 or 2");
+                continue;
+            }
+            break;
+        }
+        if(num==1){
+            readFileSlang("data/slang.txt");
+            edit_history.clear();
+            add_history.clear();
+            delete_history.clear();
+        }
+
+    }
     public void GUI(){
         while (true){
             System.out.println("\n<----------------------------------------------------------------------->");
